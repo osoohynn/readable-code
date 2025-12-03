125b80e39982fed5f9c5559079594ae28fa76fbc
su080915 <tbr335@naver.com>
2025-12-03 17:54:37 +0900

    feat: dip

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,18 +1,24 @@
 package cleancode.minesweeper.tobe;
 
-import cleancode.minesweeper.tobe.gamelevel.Advanced;
-import cleancode.minesweeper.tobe.gamelevel.Beginner;
-import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
-
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        MineSweeper mineSweeper = new MineSweeper(new VeryBeginner());
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+        GameLevel gameLevel = new VeryBeginner();
+
+        MineSweeper mineSweeper = new MineSweeper(
+                gameLevel,
+                inputHandler,
+                outputHandler
+        );
         mineSweeper.initialize();
         mineSweeper.run();
     }
